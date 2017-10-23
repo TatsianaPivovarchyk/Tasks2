0c3536eacb4df9231aae1fdb7618a5fbdda6b90f
Tatsiana Pivovarchyk <31995924+TatsianaPivovarchyk@users.noreply.github.com>
2017-10-23 23:15:41 +0300

    Add files via upload

--- Romb3/src/Application.java
+++ Romb3/src/Application.java
@@ -9,16 +9,16 @@ public class Application {
 
             Scanner in = new Scanner(System.in);
             System.out.println("Введите размер стороны ромба: ");
-            int x = in.nextInt(); //сторона ромба
-            int Size = x * 2 -1; //размер двумерного массива
+            int x = in.nextInt(); //сторона ромба,то что вводим с клавиатуры
+            int side = x * 2 -1; //длина стороны ромба -1
 
-            boolean[][] romb = new boolean[Size][Size];
+            boolean[][] romb = new boolean[side][side];
             int i, j;
             for (i = 0; i<romb.length; i++){
                 for(j = 0; j<romb.length; j++) {
-                    if (j == Size / 2 - i || j == (Size / 2 + i)) {
+                    if (j == side / 2 - i || j == (side / 2 + i)) {
                         romb[i][j] = true;
-                    } else if (j == i - Size / 2 || j == Size + Size / 2 - i - 1) {
+                    } else if (j == i - side / 2 || j == side + side / 2 - i - 1) {
                         romb[i][j] = true;
                     }
                 }
